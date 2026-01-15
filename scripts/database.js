4965a548ba00fcec46887d110ea3484b72e221f7
Douglas <supsta.jdfm@gmail.com>
2026-01-15 12:09:03 -0800

    Added SQL and Serverless Backend demos to portfolio, minor adjustments throughout.

--- scripts/database.js
+++ scripts/database.js
@@ -1,14 +1,12 @@
-// Deez database.js
-
 console.log("database.js is working");
-//noting this **I will not be installing MondoDB** 
+
 // Check if users exist in localStorage, if not, create a default admin
 if (!localStorage.getItem("users")) {
 	localStorage.setItem("users", JSON.stringify([
     { username: "admin", email: "admin@example.com", password: "password123" }
   ]));
 }
-// saving a user into mock DeezDatabase localStorage
+// saving a user into localStorage
 function saveUser(username, email, password, joinDate) {
 	const users = JSON.parse(localStorage.getItem("users")) || [];
 	users.push({ username, email, password, joinDate });
