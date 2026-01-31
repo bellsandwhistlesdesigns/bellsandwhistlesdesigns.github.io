e6fc4d1c9d5671eb2a1f743cc4acd7028d79eeac
Douglas <supsta.jdfm@gmail.com>
2026-01-31 11:52:11 -0800

    style changes and documentation

--- scripts/email.js
+++
@@ -1,6 +0,0 @@
-
-console.log("email.js is working");
-	const user = "douglasmoth";
-	const domain = "outlook.com";
-	const emailLink = document.getElementById("email");
-		emailLink.innerHTML = `<a href="mailto:${user}@${domain}">Contact me today</a>`;
